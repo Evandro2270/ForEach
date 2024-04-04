f625d0ab854e79dd395c6c3085fa3ba273b5554a
Evandro2270 <150724259+Evandro2270@users.noreply.github.com>
2024-04-04 16:16:11 -0300

    Adicionar arquivos de projeto.

---
+++ ForEach/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                int N;
+                N = int.Parse(Console.ReadLine());
+
+                string[] vet = new string[N];
+
+                for (int i = 0; i < N; i++)
+                {
+                    vet[i] = Console.ReadLine();
+                }
+                /* Console.WriteLine("Nome lidos: ");
+                for (int i = 0; i < N; i++)
+                {
+                    Console.WriteLine(vet[i]);
+                }
+                */
+                Console.WriteLine("Nome lidos: ");
+                foreach (string x in vet)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+        }
+    }
+}
